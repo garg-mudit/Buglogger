b0ed47b52ddb0816cf6db789c3a334e7b320f25c
Mudit Garg <itzmuditgarg@gmail.com>
2020-09-11 20:02:10 +0530

    Added Config for db connection

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      `mongodb+srv://muditxd:vjD17YVqNcbP39tA@bug-logger.xt7am.mongodb.net/buglogger?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      }
+    );
+
+    console.log('Mongo Db Connected');
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
